96be65a3379abed0ac861c15dc6b2bed44d673a0
Eunbi Park <69047310+bluvory@users.noreply.github.com>
2023-04-14 23:14:22 +0900

    Update 1213.py

--- 1213.py
+++ 1213.py
@@ -1,3 +1,4 @@
+# 틀림
 word = list(input())
 left, middle, right = [], [], []
 
@@ -9,7 +10,32 @@ for i in word:
     else:
         middle.append(i)
 
-if len(middle)>2:
+if len(middle)>1:
     print("I'm Sorry Hansoo")
 else:
     print(''.join(left)+''.join(middle)+''.join(reversed(right)))
+    
+# 왜틀렸냐면... 모르겟음
+# collections.Counter 써서 하기
+import collections
+
+word = list(input())
+check_word = collections.Counter(word)
+cnt = 0
+result = ''
+mid = ''
+
+for word, num in list(check_word.items()):
+    if num%2 == 1:
+        cnt += 1
+        mid = word
+        # 홀수 두개면 못함
+        if cnt >= 2:
+            print("I'm Sorry Hansoo")
+            break
+
+for word, num in sorted(check_word.items()):
+    result += (word*(num//2))
+
+if cnt < 2:
+    print(result+mid+result[::-1])
